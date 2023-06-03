4414aba751eac438ae71b47127d734e7f72440aa
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-03 20:40:34 +0200

    [feat]: added form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,8 @@ import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +18,42 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    
+    emailjs.send(service_ID, templateID, 
+      {
+        from_name: form.name,
+        to_name: 'Arturo',
+        from_email: form.email,
+        to_email: 'arturoperez.bs@gmail.com',
+        message: form.message
+      },
+      public_key
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you! I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error)
+      alert('Oops, Something went wrong.')
+    }
+    )
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>

4cc65d20f08d04d27a71586cd5f7f50daef80389
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-06 23:37:58 +0200

    [fix](carousel): removed margin top in small devices

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -32,7 +32,7 @@ const FeedbackCard = ({ testimonial, name, designation, company, image, head }) 
 const Feedbacks = () => {
   return (
     <div className='mt-12 bg-black-100 rounded-[20px]'>
-      <div className={`${styles.padding} rounded-2xl min-h-[300px]`}>
+      <div className={`${styles.padding} rounded-2xl min-h-[200px] sm:min-h-[300px]`}>
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>

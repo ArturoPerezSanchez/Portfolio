74c803731f90bdcd95a11beec214affcb068372a
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-06 23:06:49 +0200

    fix(caroussel): made it responsive

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,20 +7,21 @@ import { testimonials } from '../constants';
 
 const FeedbackCard = ({ testimonial, name, designation, company, image, head }) => (
   <>
-    <div className='ml-[50px] mr-10'>
-      <div style = {{boxShadow: "10px 10px 40px black", background: "linear-gradient(135deg, #322266, #100d25)"}} className='flex rounded-[20px] items-center mb-16'>
+    <div className='sm:ml-[50px] sm:mr-10'>
+      <div style = {{background: "linear-gradient(135deg, #322266, #100d25)"}} className='border border-sky-500 md:border-0 sm:shadow-[10px_10px_40px_rgba(0,0,0)] md:flex rounded-[20px] items-center mb-16 min-h-[500px] max-h-[500px] md:min-h-[400px] md:max-h-[400px]'>
 
-        <div style = {{marginLeft: '-50px'}} className='relative min-w-[100px] -ml-50'>
-          <img src={image} alt={`feedback by ${name}`} className='w-25 h-25 rounded-full' />
+        <div className='w-24 md:min-w-[100px] md:ml-[-50px] m-auto'>
+          <img src={image} alt={`feedback by ${name}`} className='w-16 h-24 md:min-h-[100px] md:w-25 md:h-25 mt-4 md:mt-0 rounded-full' />
         </div>
-        <div className='flex flex-col min-h-[300px] mt-10 mb-10 ml-10 mr-10'>
-          <p className='text-white font-black text-4xl font-bold mt-4'>"{head}"</p>
-          <p className='text-white tracking-wider text-lg m-auto'>{testimonial}</p>
+        <div className='flex flex-col md:min-h-[300px] mt-4 md:mt-10 ml-10 mr-10'>
+          <p className='text-white font-black text-2xl md:text-4xl font-bold mt-4'>&quot;{head}&quot;</p>
+          <p className='text-white md:text-lg mt-8 mb-8 md:m-auto'>{testimonial}</p>
             <div className='ml-4 flex justify-center items-center'>
               {/* name */}
-              <p className='text-white text-2xl text-base'> <span className='blue-text-gradient'>@</span> {name} &nbsp;| &nbsp; </p>
+              <p className='text-white text-2xl text-base'> <span className='blue-text-gradient'>@</span> {name}</p>
               {/* position */}
-              <p className='mt-1 text-gray-300 text-sm'>{designation} of {company} </p>
+              <p style={{borderRight: "solid"}}> &nbsp; &nbsp;</p>
+              <p className='mt-1 text-gray-300 text-sm'>&nbsp; &nbsp;{designation} of {company} </p>
             </div>
         </div>
       </div>
@@ -38,8 +39,8 @@ const Feedbacks = () => {
         </motion.div>
       </div>
       <div className={`${styles.paddingX} -mt-20 pb-14`}>
-        <Carousel showThumbs={false} showStatus={false} showArrows={false} infiniteLoop autoPlay>
-        {testimonials.map((testimonial, index) => (
+        <Carousel showThumbs={false} showStatus={false} showArrows={false} infiniteLoop autoPlay={false}>
+        {testimonials.map((testimonial) => (
             <div key={testimonial.name}>
               <FeedbackCard {...testimonial} />
             </div>

27ceb9e1b4eebc6e150a91333b41446f0b6e5742
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-10 19:55:44 +0200

    [fix](responsive): adjusted menu in small devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ import { useEffect, useState } from 'react';
 
 const MobileHero = () => {
   return (
-    <div className='flex flex-col relative w-full items-center'>
+    <div className='relative w-full '>
       <div className='w-2/4 absolute left-2/4'>
         <img src="https://avatars.githubusercontent.com/u/43266687?v=4" className='rounded-full w-full p-5'/>
       </div>
-      <div className='w-full bg-tertiary mr-8 p-8 rounded-3xl mt-24'>
+        <div className='w-full h-full flex flex-col bg-tertiary mr-8 p-8 rounded-3xl mt-16'>
         <a className="text-secondary" href='#about'><pre>&lt;About /&gt;</pre></a><br/>
         <a className="text-secondary" href='#work'><pre>  &lt;Experience /&gt;</pre></a><br/>
         <a className="text-secondary" href='#works'><pre>   &lt;Projects /&gt;</pre></a><br/>
@@ -43,23 +43,23 @@ const Hero = () => {
 
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
 
-        <div className='flex flex-col gap-5'>
-          <h1 className={`${styles.heroHeadText} text-white`}>👋 Welcome! <span className="text-[#915eff]">Arturo</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 p-2`}>I am a software engineer <br className='sm:block hidden'/> driven by the realms of Web Development and AI. </p>
+        <div className='flex flex-col gap-4'>
+          <h1 className={`${styles.heroHeadText} text-white`}>👋 Welcome! </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 p-2`}>I am <span className="text-[#915eff]">Arturo,</span> a software engineer <br className='sm:block hidden'/> driven by the realms of Web Development and AI. </p>
           {isDesktop ? <div></div> :  <MobileHero />}  
         </div>
       </div>
       {isDesktop ? (
         <ComputersCanvas />
       ) : (
-        <div className={`absolute bottom-14 w-full flex justify-center items-center ${scrollElementClass}`} style={{ transition: 'opacity 0.5s' }}>
-          <a href="#about">
+        <div className={`absolute bottom-14 w-full flex justify-end items-center ${scrollElementClass}`} style={{ transition: 'opacity 0.5s' }}>
+          <a href="#about" className='mr-8'>
             <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
               <motion.div 
                 animate={{

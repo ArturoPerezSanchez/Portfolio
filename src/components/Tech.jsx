0029788bc529283e94ca0eecfc46158ab4fada10
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-10 21:37:01 +0200

    [feat](balls): added animations to the images

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,27 +1,45 @@
-import { BallCanvas } from "./canvas"
-import { SectionWrapper} from "../hoc"
-import { technologies } from "../constants"
+import React, { useState } from 'react';
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 import { useMediaQuery } from 'react-responsive';
 
-
 const Tech = () => {
-  const isDesktop = useMediaQuery({ minWidth: 768 }); // Define the minimum width to consider the device as desktop
+  const isMobile = useMediaQuery({ maxWidth: 767 }); // Define the maximum width to consider the device as mobile
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const handleTouchEnd = () => {
+    setTimeout(() => {
+      setHoveredIndex(null);
+    }, 500); // (500 miliseconds)
+  };
 
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
+      {technologies.map((technology, index) => (
         <div className="w-28 h-28" key={technology.name}>
-          {isDesktop ? 
-          
-          <BallCanvas color={technology.color} icon={technology.icon}/> : 
-          
-          <img className="w-[100%] h-[100%] object-contain rounded-full" src={technology.icon}></img>
-          
-          }<div style={{background: technology.color, textShadow:"2px 0 #0005, -2px 0 #0005, 0 2px #0005, 0 -2px #0005,1px 1px #0005, -1px -1px #0005, 1px -1px #0005, -1px 1px #0005"}} className="w-full text-center rounded-full opacity-80">{technology.name}</div>
+          {isMobile ? (
+            <img
+              className={`w-[100%] h-[100%] object-contain rounded-full ${hoveredIndex === index ? 'hover-animation' : ''}`}
+              src={technology.icon}
+              onTouchStart={() => setHoveredIndex(index)}
+              onTouchEnd={() => handleTouchEnd()}
+              alt={technology.name}
+            />
+          ) : (
+            <BallCanvas color={technology.color} icon={technology.icon} />
+          )}
+          <div
+            style={{
+              background: technology.color,
+              textShadow: "2px 0 #0005, -2px 0 #0005, 0 2px #0005, 0 -2px #0005, 1px 1px #0005, -1px -1px #0005, 1px -1px #0005, -1px 1px #0005"
+            }}
+            className="w-full text-center rounded-full opacity-80"
+          >
+            {technology.name}
+          </div>
         </div>
-      ))} 
-      </div>
-  )
+      ))}
+    </div>
+  );
 }
 
 export default SectionWrapper(Tech, "tech");

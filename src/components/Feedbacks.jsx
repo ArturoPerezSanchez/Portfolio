ce983c761f4cda78ebefe07434b89f5ada944080
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-10 13:47:04 +0200

    [fix](feedback): modified margins in mobile view

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -11,20 +11,20 @@ const FeedbackCard = ({ testimonial, name, designation, company, image, head }) 
   <>
   <div className='md:ml-10 md:mr-10'>
     <div className='sm:ml-[50px] sm:mr-10'>
-      <div style = {{background: "linear-gradient(135deg, #322266, #100d25)"}} className='border border-sky-500 md:border-0 sm:shadow-[10px_10px_40px_rgba(0,0,0)] md:flex rounded-[20px] items-center mb-16 min-h-[500px] max-h-[500px] md:min-h-[400px] md:max-h-[400px]'>
+      <div style = {{background: "linear-gradient(135deg, #322266, #100d25)"}} className='border border-sky-500 md:border-0 sm:shadow-[10px_10px_40px_rgba(0,0,0)] md:flex rounded-[20px] items-center mb-16 min-h-[400px] max-h-[400px]'>
 
         <div className='w-24 md:min-w-[100px] md:ml-[-50px] m-auto'>
           <img src={image} alt={`feedback by ${name}`} className='w-16 h-24 md:min-h-[100px] md:w-25 md:h-25 mt-4 md:mt-0 rounded-full' />
         </div>
-        <div className='flex flex-col md:min-h-[300px] mt-4 md:mt-10 ml-10 mr-10'>
+        <div className='flex flex-col md:min-h-[300px] ml-4 mr-4 sm:ml-10 sm:mr-10 '>
           <p className='text-white font-black text-2xl md:text-4xl font-bold mt-4'>&quot;{head}&quot;</p>
           <p className='text-white md:text-lg mt-8 mb-8 md:m-auto'>{testimonial}</p>
-            <div className='ml-4 flex justify-center items-center'>
+            <div className='flex justify-center items-center'>
               {/* name */}
-              <p className='text-white text-2xl text-base'> <span className='blue-text-gradient'>@</span> {name}</p>
+              <p className='text-white text-2xl text-base'> <span className='blue-text-gradient'>@</span> {name}&nbsp;</p>
               {/* position */}
-              <p style={{borderRight: "solid"}}> &nbsp; &nbsp;</p>
-              <p className='text-gray-300 text-sm'>&nbsp; &nbsp;{designation} of {company} </p>
+              <p style={{borderRight: "solid"}}>&nbsp;</p>
+              <p className='text-gray-300 text-sm'>&nbsp; {designation} of {company} </p>
             </div>
         </div>
       </div>
